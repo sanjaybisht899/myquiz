bafec6a59178b1282bb4449dc8a6452eb9a161b5
Sanjay Bisht <sanjaybishtamazon@gmail.com>
2022-10-12 05:49:57 +0530

    Changes Reverted Back

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState ,useMemo} from 'react';
 import './App.css';
 import Start from './components/Start';
 import Timer from './components/Timer';
 import Trivia from './components/Trivia';
-import moneyPyramid  from './Data/pyramid';
 import data from './Data/questions'
 function App() {
 
@@ -12,7 +11,27 @@ function App() {
   const [stop, setStop] = useState(false)
   const [earned,setEarned] = useState("$ 0");
   
-
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "$ 100" },
+        { id: 2, amount: "$ 200" },
+        { id: 3, amount: "$ 300" },
+        { id: 4, amount: "$ 500" },
+        { id: 5, amount: "$ 1,000" },
+        { id: 6, amount: "$ 2,000" },
+        { id: 7, amount: "$ 4,000" },
+        { id: 8, amount: "$ 8,000" },
+        { id: 9, amount: "$ 16,000" },
+        { id: 10, amount: "$ 32,000" },
+        { id: 11, amount: "$ 64,000" },
+        { id: 12, amount: "$ 125,000" },
+        { id: 13, amount: "$ 250,000" },
+        { id: 14, amount: "$ 500,000" },
+        { id: 15, amount: "$ 1,000,000" },
+      ].reverse(),
+    []
+  );
   
   useEffect(() => {
     questionNumber >1 && 
